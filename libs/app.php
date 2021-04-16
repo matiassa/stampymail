caa4efe60a261b1bdfe7c1feddf98b4a49b50a9e
Matias Saiz <matiassandroid@gmail.com>
2021-04-16 15:02:04 -0300

    inicio

---
+++ libs/app.php
@@ -0,0 +1,21 @@
+<?php
+
+class App
+{
+    function __construct()
+    {
+        $url = isset($_GET['url']) ? $_GET['url'] : null;
+        $url = rtrim($url, '/');
+        $url = explode('/', $url);
+
+        if (empty($url[0])) {
+            error_log('APP::construct-> no hay controlador especificado');
+            $archivoController = 'controllers/login.php';
+            require_once $archivoController;
+            $controller = new Login();
+            $controller->loadModel('login');
+            $controller->render();
+            return false;
+        }
+    }
+}
